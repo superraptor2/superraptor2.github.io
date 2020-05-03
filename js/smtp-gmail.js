b2b11f3a0a59e11022ad6cdb68dc1bdc7bbed35a
superraptor2 <nico.clompen@gmail.com>
2020-05-03 16:09:22 +0200

    update

--- js/smtp-gmail.js
+++ js/smtp-gmail.js
@@ -1,5 +1,4 @@
 function youreGay() {
     console.log("No you're gay");
-    document.getElementById('img2').src = "https://superraptor2.github.io/img/Frick2.svg"
-    document.getElementById('img1').src = "https://superraptor2.github.io/img/Frick1.svg"
+    document.getElementById('talking').src = "https://superraptor2.github.io/img/Frick.svg"
 }
